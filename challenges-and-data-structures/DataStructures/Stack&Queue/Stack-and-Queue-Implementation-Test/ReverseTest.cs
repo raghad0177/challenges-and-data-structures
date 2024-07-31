ee670c151755cfff992f2abdbd18995bcc19fd4e
Raghad A. Al-yakhri <abdullahraghad616@gmail.com>
2024-07-31 18:40:32 +0300

    done all

---
+++ challenges-and-data-structures/DataStructures/Stack&Queue/Stack-and-Queue-Implementation-Test/ReverseTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_and_Queue_Implementation_Test
+{
+    public class ReverseTest
+    {
+        [Fact]
+        public void ReverseElements()
+        {
+            //Arrange
+            Stack_and_Queue_Implementation.Stack stack = new Stack_and_Queue_Implementation.Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            //Act
+            stack.ReverseStack(stack);
+            //Assert
+            Assert.Equal(1,stack.Peek());
+        }
+        [Fact]
+        public void ReverseOneElement()
+        {
+            //Arrange
+            Stack_and_Queue_Implementation.Stack stack = new Stack_and_Queue_Implementation.Stack();
+            stack.Push(1);
+            //Act
+            stack.ReverseStack(stack);
+            //Assert
+            Assert.Equal(1, stack.Peek());
+        }
+        [Fact]
+        public void ReverseEmpty()
+        {
+            //Arrange
+            Stack_and_Queue_Implementation.Stack stack = new Stack_and_Queue_Implementation.Stack();
+            //Act
+            stack.ReverseStack(stack);
+            //Assert
+            Assert.True(stack.IsEmpty());
+        }
+    }
+}
