e57c872e93722604cbee95b2ba710096786ee515
Raghad A. Al-yakhri <abdullahraghad616@gmail.com>
2024-08-05 21:14:56 +0300

    done

---
+++ challenges-and-data-structures/DataStructures/Stack&Queue/Stack-and-Queue-Implementation-Test/DeleteMiddle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_and_Queue_Implementation_Test
+{
+    public class DeleteMiddle
+    {
+        [Fact]
+        public void ReverseElements()
+        {
+            //Arrange
+            Stack_and_Queue_Implementation.Stack stack = new Stack_and_Queue_Implementation.Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            //Act
+               StringWriter stringWriter = new StringWriter();
+                TextWriter originalOutput = Console.Out;
+                Console.SetOut(stringWriter);
+                stack.DeleteMiddle(stack);
+                Console.SetOut(originalOutput);
+                string result = stringWriter.ToString().Trim();
+           // Assert
+                Assert.Equal("5 4 2 1", result);
+            
+        }
+    }
+}
