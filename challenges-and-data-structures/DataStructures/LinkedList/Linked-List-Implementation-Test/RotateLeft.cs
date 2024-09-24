2b1b35d0b561e6f6664f4922f037d3d4e9c607b9
Raghad A. Al-yakhri <abdullahraghad616@gmail.com>
2024-09-24 21:51:24 +0300

    done

---
+++ challenges-and-data-structures/DataStructures/LinkedList/Linked-List-Implementation-Test/RotateLeft.cs
@@ -0,0 +1,50 @@
+﻿using LearnLinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List_Implementation_Test
+{
+    public class RotateLeft
+    {
+        [Fact]
+        public void RotateLinkedListZero()
+        {
+            // Arreng
+            LinkedList linkedList1 = new LinkedList();
+            linkedList1.InsertLast(1);
+            linkedList1.InsertLast(2);
+            linkedList1.InsertLast(3);
+            linkedList1.InsertLast(4);
+            linkedList1.InsertLast(5);
+
+            linkedList1.RotateLeft(0);
+
+            // Assert
+            var result = linkedList1.GetLinkedListData(linkedList1);
+            Assert.Equal(new[] { 1, 2, 3, 4, 5 }, result);
+
+        }
+
+        [Fact]
+        public void RotateLinkedListSex()
+        {
+            // Arreng
+            LinkedList linkedList1 = new LinkedList();
+            linkedList1.InsertLast(1);
+            linkedList1.InsertLast(2);
+            linkedList1.InsertLast(3);
+            linkedList1.InsertLast(4);
+            linkedList1.InsertLast(5);
+
+            linkedList1.RotateLeft(6);
+
+            // Assert
+            var result = linkedList1.GetLinkedListData(linkedList1);
+            Assert.Equal(new[] { 2, 3, 4, 5, 1 }, result);
+
+        }
+    }
+}
